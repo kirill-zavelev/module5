b9fbc3f3cba2e4cf12733f21575fd83cb145a55a
ilyamakhon <ilyamakhon9135@gmail.com>
2020-04-14 21:56:48 +0300

    exception test

--- src/test/java/com/epam/testng/tests/DivOfLongValuesCalcTest.java
+++ src/test/java/com/epam/testng/tests/DivOfLongValuesCalcTest.java
@@ -2,20 +2,14 @@ package com.epam.testng.tests;
 
 import com.epam.testng.base.BaseDataProvider;
 import com.epam.testng.base.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DivOfLongValuesCalcTest extends BaseTest {
 
-    @Test(dataProvider = "DivOfTwoLongValues", dataProviderClass = BaseDataProvider.class, groups = {"longValues"})
+    @Test(dataProvider = "DivOfTwoLongValues", dataProviderClass = BaseDataProvider.class, groups = {"longValues"},
+            expectedExceptions = {NumberFormatException.class},
+            expectedExceptionsMessageRegExp = "Attempt to divide by zero")
     public void divOfTwoLongValues(long a, long b) {
-        String expectedResult = "Attempt to divide by zero";
-
-        try {
-            calculator.div(a, b);
-        } catch (NumberFormatException nfe) {
-            String actualResult = nfe.getMessage();
-            Assert.assertEquals(actualResult, expectedResult);
-        }
+        calculator.div(a, b);
     }
 }
